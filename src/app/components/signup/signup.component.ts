4f3b561c0f37094fad305566c3bc053502f6c87b
Christophe Habineza Sinayobye <sinaych@Christophes-MacBook-Air.local>
2020-09-12 18:15:40 -0500

    CHS: Heroku configurations

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { sign } from 'crypto';
 import { PwdEncryptService } from 'src/app/services/pwd-encrypt.service';
 
 @Component({
