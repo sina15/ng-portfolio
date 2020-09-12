b8f5787e265fe3507c2ad0d9ec003281b8cd9519
Christophe Habineza Sinayobye <sinaych@Christophes-MacBook-Air.local>
2020-09-12 18:09:27 -0500

    CHS: Heroku configurations

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/ANGULARDEMO'));
+app.use(express.static(__dirname + '/dist/AngularDemo'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/ANGULARDEMO/index.html'));});
+'/dist/AngularDemo/index.html'));});
 app.listen(process.env.PORT || 8080);
